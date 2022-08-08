693d7bd59d05c45cf77f67d82eb1b3946527e9e4
Semo <tpah20@gmail.com>
2022-08-09 00:58:34 +0900

    ip

--- app.py
+++ app.py
@@ -28,10 +28,10 @@ class manager_status(BaseModel):
 
     # INFER_SE: str = '0.0.0.0:8001'
     FL_client: str = '127.0.0.1:8002'
-    FL_server_ST: str = '10.152.183.248:8000'
-    FL_server: str = '10.152.183.241:8080'  # '0.0.0.1:8080'
+    FL_server_ST: str = '10.152.183.135:8000'
+    FL_server: str = '10.152.183.131:8080'  # '0.0.0.1:8080'
     # S3_filename: str = '../download_model/%s_model.h5'%today_str  # 다운로드된 모델이 저장될 위치#######################
-    S3_bucket: str = 'fl-flower-model'
+    S3_bucket: str = 'fl-gl-model'
     S3_key: str = ''  # 모델 가중치 파일 이름
     s3_ready: bool = False  # s3주소를 확보함
     FL_client_num: int = 0
